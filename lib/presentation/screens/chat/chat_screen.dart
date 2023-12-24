c5d098667de7ef0d42e28408ccea67ef052ef10a
Joel Uzcategui <Joeluzcategui102@gmail.com>
2023-12-24 09:16:36 -0400

    seccion 5 finalizada

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -40,13 +42,15 @@ class _ChatView extends StatelessWidget {
               child: ListView.builder(
                 itemCount: 100,
                 itemBuilder: (context, index) {
-                  return const MyMessageBubble();
+                  return (index % 2 == 0) ? const MyMessageBubble() : const HerMessageBubble();
               }),
             ),
-            Text('mundo'),
+            //caja de texto
+            MessageFieldBox(),
           ],
         ),
       ),
     );
   }
 }
+
